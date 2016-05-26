8c77539989f3da9dd6f31f5145932c9ec83e3baa
pavel.akira.marek@gmail.com <pavel.akira.marek@gmail.com>
2016-05-26 10:57:55 +0200

    Odebrany typovy parametr pro Node, ted je pevne (Node String) Trideni hran pomoci quicksortu, vypisovani hran a vrcholu

--- kruskal.hs
+++ kruskal.hs
@@ -4,35 +4,47 @@ import Data.List
 data Cislo = Cislo Int deriving (Show)
 
 
-data Graph t = Graph [Node t] [Edge t]
+data Graph = Graph [Node] [Edge]
 
-data Node t = Node t
+data Node = Node String
 
 {- Edge Zacatek Konec Cena -}
-data Edge t = Edge (Node t) (Node t) Int
+data Edge = Edge Node Node Int
 
 
-instance (Eq t) => Eq (Node t) where
+instance Eq Node where
     (Node a) == (Node b) = a == b
 
 {- Potrebujeme zadefinovat operator >,
 -- abychom mohli na zacatku Kruskalova
 -- algoritmu setridit vsechny hrany
 -- podle ceny. -}
-instance (Eq t) => Ord (Edge t) where
+instance Ord Edge where
     (Edge _ _ x) <= (Edge _ _ y) = x <= y
 
 
-instance (Eq t) => Eq (Edge t) where
+instance Eq Edge where
     (Edge node1 node2 _) == (Edge node3 node4 _) =
         node1 == node3 && node2 == node4
 
+instance Show Edge where
+    show (Edge node1 node2 price) = "("++(show node1)++"->"++(show node2)++","++(show price)++")"
 
 
-edgeSort :: (Eq t) => [Edge t]-> [Edge t]
+instance Show Node where
+    show (Node s) = s
+
+
+edgeSort :: [Edge]-> [Edge]
 edgeSort []     = []
 edgeSort (x:xs) = (edgeSort [y|y<-xs, y<x]) ++ [x] ++ (edgeSort [y|y<-xs, y>=x])
 
-{- VSTUPY -}
-edges1 :: Char -> [Edge String]
-edges1 c = [(Edge (Node "a") (Node "b") 10)] ++ [(Edge (Node "b") (Node "c") 5)]
+
+{- HRANY A VRCHOLY -}
+edges1 :: () -> [Edge]
+edges1 () = [(Edge (Node "a") (Node "b") 10)] ++ [(Edge (Node "b") (Node "c") 5)] ++ [(Edge (Node "c") (Node "a") 1)]
+
+nodes1 :: ()-> [Node]
+nodes1 () = [(Node "a")] ++ [(Node "b")]
+
+
