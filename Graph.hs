c9c6a58cbdefd19a26ab28c93b159ad7b044f349
pavel.akira.marek@gmail.com <pavel.akira.marek@gmail.com>
2016-06-01 19:20:51 +0200

    Final version.

--- Graph.hs
+++ Graph.hs
@@ -1,5 +1,4 @@
 module Graph where
-import Data.List
 
 data Graph = Graph [Node] [Edge] deriving (Show)
 
@@ -8,22 +7,28 @@ data Node = Node String
 instance Eq Node where
     (Node a) == (Node b) = a == b
 
+instance Ord Node where
+    (Node a) <= (Node b) = a < b
+
+instance Show Node where
+    show (Node s) = s
 
 instance Read Node where
     readsPrec _ str = [((Node str),[])]
 
 
-{- Edge Zacatek Konec Vaha -}
+-- Edge Zacatek Konec Vaha
 data Edge = Edge Node Node Float
 
-{- Potrebujeme zadefinovat operator >,
--- abychom mohli na zacatku Kruskalova
--- algoritmu setridit vsechny hrany
--- podle ceny. -}
+-- potrebujeme zadefinovat usporadani hran, abychom
+-- je na zacatku kruskalova algoritmu mohli vsechny
+-- setridit.
 instance Ord Edge where
     (Edge _ _ x) <= (Edge _ _ y) = x <= y
 
 
+-- hrany jsou stejne, pokud maji stejny zacatecni a 
+-- koncovy vrchol.
 instance Eq Edge where
     (Edge node1 node2 _) == (Edge node3 node4 _) =
         node1 == node3 && node2 == node4
@@ -32,47 +37,7 @@ instance Show Edge where
     show (Edge node1 node2 weight) = "("++(show node1)++"->"++(show node2)++","++(show weight)++")"
 
 
-instance Show Node where
-    show (Node s) = s
-
-
+-- trideni hran implementovane pomoci quicksortu.
 edgeSort :: [Edge]-> [Edge]
 edgeSort []     = []
 edgeSort (x:xs) = (edgeSort [y|y<-xs, y<x]) ++ [x] ++ (edgeSort [y|y<-xs, y>=x])
-
-
-
--- === IO === ---
-
-
-
-
-
-{-  HRANY A VRCHOLY -}
-
-
-
-edges1 :: () -> [Edge]
-edges1 () = [(Edge (Node "a") (Node "b") 2)] ++ [(Edge (Node "b") (Node "c") 3)] ++ [(Edge (Node "c") (Node "a") 1)]
-
-nodes1 :: ()-> [Node]
-nodes1 () = [(Node "a")] ++ [(Node "b")] ++ [(Node "c")]
-
---trojuhelnik
-graph1 ::()-> Graph
-graph1 () = Graph (nodes1 ()) (edges1 ())
-
-node1 :: ()-> Node
-node1 () = (Node "x")
-
-
-graph2 ::()-> Graph
-graph2 () = Graph (nodes2 ()) (edges2 ())
-
-nodes2 :: ()-> [Node]
-nodes2 () = [(Node "a")] ++ [(Node "b")] ++ [(Node "c")] ++ [(Node "d")] ++ [(Node "e")]
-
-edges2 :: () -> [Edge]
-edges2 () = [(Edge (Node "a") (Node "b") (2.5))] ++ [(Edge (Node "a") (Node "e") 2)] ++ [(Edge (Node "a") (Node "d") 10)] ++
-    [(Edge (Node "b") (Node "c") 3)] ++ [(Edge (Node "b") (Node "d") 1)] ++ [(Edge (Node "c") (Node "d") 8)] ++
-    [(Edge (Node "c") (Node "e") 4)] ++ [(Edge (Node "d") (Node "e") 5)]
--- Kruskal.hs
+++ Kruskal.hs
@@ -2,23 +2,32 @@ module Kruskal where
 
 import Graph
 import System.IO
+import qualified Data.Set as Set
 
---Skeleton musi mit take informaci o tom, jake hrany
---jsme do nej pridali.
+
+-- Skeleton musi mit take informaci o tom, jake hrany
+-- jsme do nej pridali.
 data Skeleton = Skeleton {
     components :: Components,
     edges :: [Edge]
     }
 
---reprezentace komponenty pomoci seznamu vrcholu. Pro lepsi casovou
---slozitost by bylo lepsi implementovat je pomoci Set.
-type Component = [Node]
 
---Komponenta ma svoje ID a obsah. Ve finale komponenty vypadaji napr.
---takto: [(1,[A]),(2,[B,C]),(3,[D])]. Pozdeji s komponentami manipulujeme
---pomoci ID - viz unionComponents, deleteComponent.
+-- reprezentace komponenty pomoci Setu vrcholu. Set umoznuje logaritmickou
+-- slozitost pri praci nad timto typem.
+type Component = Set.Set Node
+
+
+-- Komponenta ma svoje ID a obsah. Ve finale komponenty vypadaji napr.
+-- takto: [(1,[A]),(2,[B,C]),(3,[D])], kde A je zkratka za (Node "A").
+-- Pozdeji s komponentami manipulujeme pomoci ID - viz unionComponents, 
+-- deleteComponent. Z praktickych duvodu je tento typ implementovan 
+-- pomoci seznamu.
 type Components = [(Int, Component)] 
 
+
+-- vypis kostry je definovan tak, ze se vypise seznam vsech hran, ktere tato
+-- kostra obsahuje.
 instance Show Skeleton where
     show skeleton = show $ edges $ skeleton
 
@@ -27,102 +36,112 @@ instance Show Skeleton where
 {-                            FIND                                  -}
 {-==================================================================-}
 
---vrati cislo komponenty, ve ktere je dany vrchol
-findInComponents :: Components->Node-> Int
-findInComponents (x:xs) node
-    | findInComponent (snd x) node = fst x --vrchol je v x, vracime index x
-    | otherwise                    = findInComponents xs node
 
-
-findInComponent :: Component->Node-> Bool
-findInComponent [] _ = False --prosli jsme celou komponentu a nic jsme nenasli
-findInComponent (x:xs) node
-    | x == node = True
-    | otherwise = findInComponent xs node
-
-
---zjisti, jestli jsou dane vrcholy ve stejne komponente
+-- V dane kostre zjisti, jestli jsou dane vrcholy ve stejne komponente.
+-- True vraci v pripade, ze jsou ve stejne komponente.
 find :: Node->Node->Skeleton-> Bool
 find u v skeleton | (findInComponents comps u) == (findInComponents comps v) = True --jsou ve stejne komponente
                   | otherwise                                                = False
                   where
                       comps = components skeleton
 
+
+-- prohleda seznam vsech komponent a vrati ID te komponenty, ve ktere
+-- se vyskytuje zadany vrchol.
+findInComponents :: Components->Node-> Int
+findInComponents (x:xs) node
+    | findInComponent (snd x) node = fst x --vrchol je v x, vracime index x
+    | otherwise                    = findInComponents xs node
+
+
+-- V dane komponente hleda dany vrchol.
+findInComponent :: Component->Node-> Bool
+findInComponent comp node = Set.member node comp
+
+
 {-==================================================================-}
 {-                           UNION                                  -}
 {-==================================================================-}
 
---vyhleda indexy obou vrcholu
---vraci nove vytvorenou kostru
+-- Prida danou hranu do dane kostry. Coz znamena, ze spoji komponenty
+-- obou koncovych vrcholu. Predpoklada se, ze koncove vrcholy jsou v 
+-- ruznych komponentach. Implementovano tak, ze vytvori novou kostru 
+-- ze stare.
 union :: Edge->Skeleton-> Skeleton
 union (Edge u v w) skeleton = 
     let
         comps = components skeleton
-        oldEdges = edges skeleton
+        oldEdges = edges skeleton --uloz hrany
         i = findInComponents comps u --index komponenty, ve ktere je vrchol u
         j = findInComponents comps v --index komponenty, ve ktere je vrchol v, predpoklada se, ze i!=j
     in
         Skeleton {
             components = unionComponents comps i j, --spoj komponenty i a j do sebe
-            edges = oldEdges ++ [(Edge u v w)]
-            --ostatni parametry noveho Skeletonu
+            edges = oldEdges ++ [(Edge u v w)] --pridej nove vzniklou hranu mezi vrcholy u, v
         }
 
 
---podle indexu komponent zapoj jednu komponentu do druhe
---je jedno, kterou komponentu zapojujeme do ktere
+-- podle indexu komponent zapoj jednu komponentu do druhe. Na pevno
+-- zapojujeme druhou zadanou komponentu do prvni, protoze na poradi
+-- nezalezi. V ramci zachovani jednoznacnosti ID komponent bude mit
+-- komponenta vznikla spojenim dvou zadanych komponent ID prvni 
+-- zadane komponenty.
 unionComponents :: Components->Int->Int-> Components
 unionComponents comps i j =
     let
         compi  = getComponent comps i
         compj  = getComponent comps j
-        newComp  = joinComponents compi compj
-        --index = (length comps) + 1 --index takhle neni jednoznacny
+        newComp  = joinComponents compi compj --spoj komponenty do sebe
         tmpComps = deleteComponent comps j --smaz j-tou komponentu ze seznamu komponent
-        newComps = deleteComponent tmpComps i --smaz i-tou komponentu
+        newComps = deleteComponent tmpComps i --smaz i-tou komponentu ze seznamu komponent
     in
         addComponent newComps (Just i) newComp  --do seznamu, ze ktereho jsme smazali i-tou a j-tou komponentu pridame newComp
 
 
---smaze i-tou komponentu, vraci seznam komponent bez teto komponenty
+-- smaze i-tou komponentu, vraci seznam komponent bez teto komponenty.
 deleteComponent :: Components->Int-> Components
 deleteComponent comps index = deleteComponent_acc comps index []
 
---s akumulatorem
+-- mazani komponent pomoci akumulatoru.
 deleteComponent_acc :: Components->Int->Components-> Components
 deleteComponent_acc (x:xs) index acc
-    | fst x == index = acc ++ xs  --tuhle komponentu chci smazat
-    | otherwise      = deleteComponent_acc xs index (acc ++ [x])
+    | fst x == index = acc ++ xs  --tuhle komponentu chci smazat - vrat akumulator a zbytek seznamu
+    | otherwise      = deleteComponent_acc xs index (acc ++ [x]) 
 
 
---prida komponentu do seznamu komponent.
---index nove komponenty bude o jedna vetsi, nez je 
---index posledni komponenty v Components,
---nebo tento index posleme jako argument
+-- prida komponentu do seznamu komponent. ID teto komponenty zvoli
+-- podle druheho argumentu - pokud je to Nothing, tak 
+-- ID = (velikost seznamu komponent) + 1
+-- a pokud je to (Just index) tak ID = index.
 addComponent :: Components->Maybe Int->Component-> Components
 addComponent comps Nothing comp      = comps ++ [((length comps) + 1,comp)]
 addComponent comps (Just index) comp = comps ++ [(index,comp)] 
 
 
---zapoji druhou do prvni.
+-- zapoji druhou komponentu do prvni.
 joinComponents :: Component->Component-> Component
-joinComponents comp1 comp2 = comp1 ++ comp2
+joinComponents comp1 comp2 = Set.union comp1 comp2
 
 
---vrati i-tou komponentu
+-- ze zadaneho seznamu vsech komponent vraci komponentu se 
+-- zadanym ID.
 getComponent :: Components->Int-> Component
 getComponent (x:xs) index 
     | (fst x) == index = snd x
     | otherwise        = getComponent xs index
 
 
-
---zkusi pridat hranu do kostry, vraci novou kostru
+-- zkusi pridat zadanou hranu do zadane kostry, vraci novou kostru.
+-- pokud jsou koncove vrcholy hrany ve stejne komponente, tak nic 
+-- neprida, pokud ne, tak tuto hranu do kostry prida.
 addEdge :: Edge->Skeleton-> Skeleton
 addEdge (Edge u v w) skeleton | find u v skeleton = skeleton                     --u a v jsou ve stejne komponente
                               | otherwise         = union (Edge u v w) skeleton  --nejsou ve stejne komponente
 
---ukoncujici podminka pro kruskala, spocita m == n-1
+
+-- ukoncujici podminka pro kruskala, ze zadane kostry spocita 
+-- m == n-1. Pokud ano, tak uz nema smysl zadnou hranu do kostry
+-- pridavat.
 enoughEdges :: Skeleton-> Bool
 enoughEdges skeleton = 
     let 
@@ -133,32 +152,33 @@ enoughEdges skeleton =
         then True
         else False
 
---spocita, kolik ma dana kostra vrcholu.
---secte delky vsech komponent
+
+-- spocita, kolik ma dana kostra vrcholu. Implementovano tak, ze
+-- pomoci akumulatoru secte delky vsech komponent.
 nodeCount :: Skeleton-> Int
 nodeCount skeleton = nodeCount_acc (components skeleton) 0
+
 
 nodeCount_acc :: Components->Int-> Int
 nodeCount_acc [] acc     = acc
 nodeCount_acc (x:xs) acc =
     let
-        nodes = snd x
-        i = length nodes
+        nodes = snd x --komponenta x
+        i = length nodes --velikost komponenty x
     in
         nodeCount_acc xs (acc + i)
 
---existsCycle :: Skeleton-> Bool
 
---z grafu vytvori kostru tak, ze kazdy vrchol da do sve vlastni komponenty.
+-- z grafu vytvori kostru tak, ze kazdy vrchol da do sve vlastni komponenty.
 newSkeleton :: Graph-> Skeleton
 newSkeleton g = newSkeleton_acc g [] 
 
---s akumulatorem
+
 newSkeleton_acc :: Graph->Components-> Skeleton
-newSkeleton_acc (Graph [] _) acc     = Skeleton {components = acc, edges = []}
+newSkeleton_acc (Graph [] _) acc     = Skeleton {components = acc, edges = []} --nova kostra nema jeste zadne hrany
 newSkeleton_acc (Graph (x:xs) e) acc =
     let
-        newAcc = addComponent acc Nothing [x] --do akumulatoru pridame novou komponentu, ktera ma v sobe jen vrchol x
+        newAcc = addComponent acc Nothing (Set.fromList [x]) --do akumulatoru pridame novou komponentu, ktera ma v sobe jen vrchol x
     in
         newSkeleton_acc (Graph xs e) newAcc --rekurzime se na graf s odebranym vrcholem
 
@@ -167,40 +187,22 @@ newSkeleton_acc (Graph (x:xs) e) acc =
 {-                           KRUSKAL                                -}
 {-==================================================================-}
 
+
 kruskal :: Graph-> Skeleton
-kruskal (Graph v e) = func1 edgeFront (newSkeleton (Graph v e))
+kruskal (Graph v e) = step edgeFront (newSkeleton (Graph v e))
                     where
                         edgeFront = edgeSort e --fronta hran serazena podle vahy
 
---vykousne prvni hranu z fronty a zkusi ji pridat do kostry
-func1 :: [Edge]->Skeleton-> Skeleton
-func1 [] skeleton     = skeleton --skoncili jsme
-func1 (x:xs) skeleton =
+
+-- ze zadane fronty hran, setridene podle vahy, vykousne jednu hranu
+-- a zkusi ji pridat do zadane kostry. Vraci nove vzniklou kostru.
+-- tato funkce predstavuje jeden krok kruskalova algoritmu.
+step :: [Edge]->Skeleton-> Skeleton
+step [] skeleton     = skeleton --skoncili jsme
+step (x:xs) skeleton =
     let 
-        newSkel = addEdge x skeleton
+        newSkel = addEdge x skeleton --z fronty vykousneme prvni hranu a zkusime ji pridat do kostry
     in
-        --writeSkeleton newSkel --debug
         if (enoughEdges newSkel)
-        then func1 [] newSkel --zbytek hranove fronty jsme odstrihli, protoze ho uz nebudeme potrebovat
-        else func1 xs newSkel
-
-
---debug
-writeSkeleton :: Skeleton -> IO ()
-writeSkeleton skeleton = putStrLn $ show $ skeleton
-
-comps1 :: ()-> Components
-comps1 () = [(1,[(Node "A")])] ++ [(2,[(Node "B"), (Node "C")])]
-
-comps2 :: ()-> Components
-comps2 () = [(3,[(Node "c")])] ++ [(4,[(Node "d")])] ++ [(5,[(Node "a"), (Node "b")])]
-
-edges3 :: () -> [Edge]
-edges3 () = [(Edge (Node "a") (Node "b") (1.0))]
-
-skeleton1 :: ()-> Skeleton
-skeleton1 () = Skeleton {components = (comps2 ()), edges = (edges3 ())}
-
-component1 ::()-> Component
-component1 () = [(Node "D")]
-
+        then step [] newSkel --"nasilne" ukoncime funkci
+        else step xs newSkel 
